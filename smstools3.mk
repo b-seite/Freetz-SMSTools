4196fb58e1e3806a345e68454271e848362e0a1f
Boris Beplate <boris@ubuntu.(none)>
2012-03-16 07:53:01 +0100

    mod on thirsday evening

--- smstools3.mk
+++ smstools3.mk
@@ -1,34 +1,31 @@
 $(call PKG_INIT_BIN, 3.1.14)
 $(PKG)_SOURCE := $(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5 := e0f9f84240f0db9a286aa3a5fa3bd8fb
-$(PKG)_SITE := http://smstools3.kekekasvi.com/packages/
+$(PKG)_SITE := http://smstools3.kekekasvi.com/packages
 
-$(PKG)_BINARY:=$($(PKG)_DIR)/smstools3
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/smstools3
-
-
-PTHREAD_LDFLAGS := -lpthread
-
-$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/smsd
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/smsd
 
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
-$(PKG_CONFIGURED_CONFIGURE)
+$(PKG_CONFIGURED_NOP)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(SMSTOOLS3_DIR) -f makefile.unix CXX="$(TARGET_CXX)" CXXFLAGS="$(TARGET_CFLAGS)" DEFINES="" LDFLAGS="$(SMSTOOLS3_LDFLAGS)"
-	
-$($(PKG)_TARGET_BINARY): $($PKG)_BINARY)
-		$(INSTALL_BINARY_STRIP)
+	$(SUBMAKE) -C $(SMSTOOLS3_DIR) \
+		CC="$(TARGET_CC)" 
+		
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
 
 $(pkg):
 
 $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
 $(pkg)-clean:
-	-$(SUBMAKE) -C $(SMSTOOLS3_DIR)  -f makefile.unix clean
-	
+	-$(SUBMAKE) -C $(SMSTOOLS3_DIR)
+	$(RM) $(SMSTOOLS3_DIR)/.configured
 
 $(pkg)-uninstall:
 	$(RM) $(SMSTOOLS3_TARGET_BINARY)
